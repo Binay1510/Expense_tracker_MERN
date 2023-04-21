bc90353d4e99a21339cb653da3e49b20c891a12c
Binay <binaypal105@gmail.com>
2023-04-21 23:41:58 +0530

    update files

--- frontend/src/context/gloablContext.js
+++ frontend/src/context/gloablContext.js
@@ -1,4 +1,6 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from 'axios'
+const BASE_URL="http://localhost:5000/api/v1/";
 
 
 
@@ -6,9 +8,22 @@ const GlobalContext=React.createContext()
 
 
 export const GlobalProvider=({children})=>{
+
+    const[incomes ,setIncomes]=useState([])
+    const[expenses ,setExpenses]=useState([])
+    const[error,serError]=useState(null)
+
+    const addIncome=async (income)=>{
+        const response=await axios.post(`${BASE_URL} add-income`,income)
+        .catch((err)=>{
+            serError(err.response.data.message)
+        })
+    }
+
+
     return(
-    <GlobalContext.Provider>
+    <GlobalContext.Provider value={'hello'}>
         {childern}
     </GlobalContext.Provider>
 )
-}
+} 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import { GlobalStyle } from './styles/GlobalStyle';
+import { GlobalProvider } from './context/gloablContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
+    <GlobalProvider>
     <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
 
