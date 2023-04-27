2cf3bf68078c96611e954417e0943c49113d3ae0
Binay <binaypal105@gmail.com>
2023-04-27 19:02:30 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -3,15 +3,18 @@ const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
 const {readdirSync} = require('fs')
+const app=express()
 
 require('dotenv').config()
-const PORT=process.env.PORT
+const PORT=process.env.PORT || 5000;
 
 
 
 //middlewares
 app.use(express.json())
 app.use(cors())
+
+//const con=require('./db/db')
 
 //routes
 readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
--- backend/routes/transaction.js
+++ backend/routes/transaction.js
@@ -6,9 +6,9 @@ const router= require('express').Router()
 router.post('/add-income',addIncome)
 
 
-//router.post('/add-income',addIncome )
+// //router.post('/add-income',addIncome )
     
-/*.post('/add-expense', addExpense)
-.get('/get-expenses', getExpense)
-.delete('/delete-expense/:id', deleteExpense)*/
-module.exports=router
+// /*.post('/add-expense', addExpense)
+// .get('/get-expenses', getExpense)
+// .delete('/delete-expense/:id', deleteExpense)*/
+module.exports=router 
